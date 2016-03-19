5366c3b12b44aef9c5c50c0944f35afa624d35f1
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-03-19 21:36:35 +0800

    Added logo

--- gulpfile.js
+++ gulpfile.js
@@ -140,6 +140,18 @@ function buildfont() {
       .pipe(gulp.dest('./build'));
 }
 
+function buildassets() {
+  return gulp.src('./assets/**/*.*')
+      .pipe(rev())
+      .pipe(gulp.dest('./build/assets'))
+      .pipe(rev.manifest({
+        path: './build/rev-manifest.json',
+        base: './build',
+        merge: true
+      }))
+      .pipe(gulp.dest('./build'));
+}
+
 function buildrev() {
   function replaceMap(filename) {
     if(filename.indexOf('.map') > -1) {
@@ -181,6 +193,10 @@ gulp.task('clean:font', function() {
   return del('dist/fonts/**/*.*', 'build/fonts/**/*.*');
 });
 
+gulp.task('clean:assets', function() {
+  return del('dist/assets/**/*.*', 'build/assets/**/*.*');
+});
+
 gulp.task('clean:index', function() {
   return del('dist/index.html');
 });
@@ -189,13 +205,14 @@ gulp.task('build:js', gulp.series('clean:js', buildjs));
 gulp.task('build:css', gulp.series('clean:css', buildcss));
 gulp.task('build:dep', gulp.series('clean:dep', builddep));
 gulp.task('build:font', gulp.series('clean:font', buildfont));
+gulp.task('build:assets', gulp.series('clean:assets', buildassets));
 gulp.task('build:index', gulp.series('clean:index', function() {
   return gulp.src('./html/index.html')
       .pipe(gulp.dest('./build'));
 }));
 gulp.task('build:rev', gulp.series(buildrev));
 
-gulp.task('build', gulp.series(gulp.series('build:font', 'build:dep', 'build:js', 'build:css', 'build:index'), 'build:rev'));
+gulp.task('build', gulp.series(gulp.series('build:font', 'build:assets', 'build:dep', 'build:js', 'build:css', 'build:index'), 'build:rev'));
 gulp.task('build:production', gulp.series('prebuild:production', 'build'));
 gulp.task('watch', gulp.series('build', function(done) {
   gulp.watch(['./ts/**/*.ts', './html/view/**/*.html', './html/tmpl/**/*.html'], gulp.series('build:js', 'build:rev'));
