1444bd161a63b40ab2444fb56b70cea13ba0196e
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-05-09 20:12:35 +0800

    Use precompiled angular libs

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,16 @@ module.exports = {
     root: __dirname,
     extensions: ['', '.ts', '.js'],
     modulesDirectories: ['node_modules'],
+    alias: {
+      '@angular/common': '@angular/common/common.umd.js',
+      '@angular/compiler': '@angular/compiler/compiler.umd.js',
+      '@angular/core': '@angular/core/core.umd.js',
+      '@angular/http': '@angular/http/http.umd.js',
+      '@angular/platform-browser': '@angular/platform-browser/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': '@angular/platform-browser-dynamic/platform-browser-dynamic.umd.js',
+      '@angular/router': '@angular/router/router.umd.js',
+      '@angular/router-deprecated': '@angular/router-deprecated/router-deprecated.umd.js',
+    },
   },
 
   module: {
